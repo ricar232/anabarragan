8555ea7787a2976932a87f4adfb14e292934fdbf
ricar233 <motatorresrichard@gmail.com>
2025-01-16 17:52:52 -0500

    test

--- php/conexion.php
+++ php/conexion.php
@@ -9,8 +9,8 @@ try {
     $host = 'localhost';
     $port = 3306; // Cambiar si usas otro puerto
     $dbname = 'segurosanabarragan';
-    $username = 'Ana';
-    $password = 'ana*123';
+    $username = 'brenda';
+    $password = 'brenda*123';
 
     // Crear conexión PDO
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
