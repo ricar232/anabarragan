f94cb215e80e6683fbcd92de58121bdb2f0a21f0
ricar233 <motatorresrichard@gmail.com>
2025-01-16 23:29:48 -0500

    test

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-session_destroy();
-header("Location: index.php");
+session_unset(); // Limpia todas las variables de sesión
+session_destroy(); // Destruye la sesión
+
+// Redirigir al login después de cerrar la sesión
+header('Location: index.php');
 exit();
 ?>
